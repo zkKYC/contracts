ef329e3a1466f13fbece7b126fa6cadbe3a952eb
Sergey <mysteryon88@yandex.ru>
2024-05-28 18:24:10 +0300

    update

--- contracts/verifiers/IVerifiers.sol
+++ contracts/verifiers/IVerifiers.sol
@@ -1,38 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-interface IAgeVerifier {
-    function verifyProof(
-        uint[2] calldata _pA,
-        uint[2][2] calldata _pB,
-        uint[2] calldata _pC,
-        uint[4] calldata _pubSignals
-    ) external view returns (bool r);
-}
-
 interface ICommitmentVerifier {
     function verifyProof(
         uint[2] calldata _pA,
         uint[2][2] calldata _pB,
         uint[2] calldata _pC,
         uint[1] calldata _pubSignals
-    ) external view returns (bool);
-}
-
-interface IHashEqualityVerifier {
-    function verifyProof(
-        uint[2] calldata _pA,
-        uint[2][2] calldata _pB,
-        uint[2] calldata _pC,
-        uint[4] calldata _pubSignals
-    ) external view returns (bool);
-}
-
-interface IBlindHashEqualityVerifier {
-    function verifyProof(
-        uint[2] calldata _pA,
-        uint[2][2] calldata _pB,
-        uint[2] calldata _pC,
-        uint[3] calldata _pubSignals
     ) external view returns (bool);
 }
